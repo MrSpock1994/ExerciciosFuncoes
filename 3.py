3ef462c2319e2a66b93a8e38104c9f3e9b17731a
MrSpock1994 <willpokerpro2016@gmail.com>
2021-12-14 21:43:26 -0300

    "First commit"

---
+++ 3.py
@@ -0,0 +1,14 @@
+"""
+Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+
+"""
+
+
+def soma(a, b, c):
+    return print(a + b + c)
+
+
+n1 = float(input('Digite o primeiro numero: '))
+n2 = float(input('Digite o segundo numero: '))
+n3 = float(input('Digite o terceiro numero: '))
+soma(n1, n2, n3)
